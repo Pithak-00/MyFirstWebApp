98e1ce89aa0e0c8c0fe08171679774d513e31383
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2023-07-03 16:24:53 +0900

    Category取得

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MyFirstWebApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +8,14 @@ namespace MyFirstWebApp.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: /<controller>/
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Catagories.ToList();
             return View();
         }
     }
