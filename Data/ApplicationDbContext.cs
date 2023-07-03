57481b6051849fe6d071a8906a51dd8151ed0d14
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2023-07-03 16:14:26 +0900

    データ追加

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,15 @@ namespace MyFirstWebApp.Data
 		}
 
 		public DbSet<Category> Catagories { get; set; }
-	}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+				new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+				new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+				new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+				);
+		}
+    }
 }
 
