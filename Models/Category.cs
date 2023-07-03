fcd2f5c09dadf83b009c4651892f8d19c421212a
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2023-07-03 10:54:56 +0900

    Categoryモデル作成

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+namespace MyFirstWebApp.Models
+{
+	public class Category
+	{
+		public Category()
+		{
+		}
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+
+	}
+}
+
