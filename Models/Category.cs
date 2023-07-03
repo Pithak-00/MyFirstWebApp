814dabfa7e8be1789fad0fe8bac02ece484a0c3b
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2023-07-03 10:59:31 +0900

    データの注釈

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,13 +1,12 @@
-﻿ using System;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace MyFirstWebApp.Models
 {
 	public class Category
 	{
-		public Category()
-		{
-		}
-
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public string Name { get; set; }
 		public int DisplayOrder { get; set; }
 
